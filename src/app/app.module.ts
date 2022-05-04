92164b756a8434e0829b56a0e5a80a9fc237176f
rustwolf <azhar.mwm@gmail.com>
2022-05-04 12:59:09 +0530

    Fixing blocking images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LinechartComponent } from './linechart/linechart.component';
 
+
+// Trying Standalone components from angular 14
 @NgModule({
   declarations: [
     AppComponent,
