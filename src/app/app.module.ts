50dd970287622fe32169049bd0edd10cedad5cc9
Azhar <azhar.mwm@gmail.com>
2022-11-07 21:07:18 +0530

    Imported standalone component in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LinechartComponent } from './linechart/linechart.component';
 import { LearningRxjsComponent } from './learning-rxjs/learning-rxjs.component';
+import { HelloStandaloneComponent } from './hello-standalone/hello-standalone.component';
 
 
 // Trying Standalone components from angular 14
@@ -14,7 +15,8 @@ import { LearningRxjsComponent } from './learning-rxjs/learning-rxjs.component';
     LearningRxjsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HelloStandaloneComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
