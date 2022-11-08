d504fb79254b9f8338407a41418c4ca6de4021ec
Azhar <azhar.mwm@gmail.com>
2022-11-08 23:30:05 +0530

    Ṁaking Http calls from standalone component

--- src/app/hello-standalone/hello-standalone.component.ts
+++ src/app/hello-standalone/hello-standalone.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hello-standalone',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './hello-standalone.component.html',
   styleUrls: ['./hello-standalone.component.css']
 })
 export class HelloStandaloneComponent implements OnInit {
+  data: any;
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .pipe().subscribe(data => {
+        this.data = data;
+      })
   }
 
 }
