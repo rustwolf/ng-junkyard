4abd981e275f3db58736c137a88d8a3fd823fa25
Azhar <azhar.mwm@gmail.com>
2022-12-17 22:47:55 +0530

    Experimenting Overlay

--- src/app/hello-standalone/hello-standalone.component.ts
+++ src/app/hello-standalone/hello-standalone.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-hello-standalone',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, OverlayModule],
   templateUrl: './hello-standalone.component.html',
   styleUrls: ['./hello-standalone.component.css']
 })
 export class HelloStandaloneComponent implements OnInit {
   data: any;
 
+  isOpen = false;
   constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
