492aa05a194b1b774f3cdce107a8124faf46d701
Azhar Office <azhar.mwm@gmail.com>
2024-09-17 23:47:04 +0530

    Added Contact Us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { CustomersComponent } from '@component/customers/customers.component';
 import { UsersComponent } from '@component/users/users.component';
+import { ContactUsComponent } from '@component/contact-us/contact-us.component';
 
 export const routes: Routes = [
     { path: 'users', component: UsersComponent }, 
-    { path: 'customers', component: CustomersComponent }
+    { path: 'customers', component: CustomersComponent },
+    { path : 'contactus', component: ContactUsComponent}
 ];
 
 @NgModule({
